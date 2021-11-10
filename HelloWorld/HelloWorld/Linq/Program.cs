3e5cfc3dbc78e9009b1b0489e7b7c090f4d2fdfd
Mirek <avax@seznam.cz>
2021-11-10 15:06:28 +0100

    doplněné úkoly na Linq

--- HelloWorld/HelloWorld/Linq/Program.cs
+++ HelloWorld/HelloWorld/Linq/Program.cs
@@ -7,7 +7,8 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, -30, 4, 20, 256, -45, 99 };
+
+            //int[] numbers = { 1, 2, -30, 4, 20, 256, -45, 99 };
 
             //where filtruje kolekci na cisla >= 0
             #region Where
@@ -57,11 +58,77 @@ namespace Linq
             */
             #endregion
 
-            //dalsi fce
-
+            //Sum, Max, Min, Average, First, Last,... dalsi fce
+            #region Sum_a_dalsi
+            /*
             var resultSum = numbers.Sum(); //suma cisel, vraci int, stejne by slo pouzit napr. Max, Min, Average
 
-            Console.WriteLine(result);
+            var resultFirst = numbers.First(); //vypsani prvniho prvku, pomoci Last() lze vypsat posledni prvek
+
+            Console.WriteLine(resultSum);
+            Console.WriteLine(resultFirst);
+            */
+            #endregion
+
+
+            //Samostatná práce
+            #region Zadani_reseni
+            /*
+            int[] numbers = { -2079, -498, 2920, -1856, 332, -2549, -674, -120, -992, 2782, 320, -524, 135, 952, 1868, 2509, -230, -138, -904, -480 };
+
+            /// z "numebers" zjistěte:
+            /// 1. počet prvků v poli
+            var pocet = numbers.Count();
+            Console.WriteLine($"Počet: {pocet}");
+            /// 2. největší hodnotu
+            var maximum = numbers.Max();
+            Console.WriteLine($"Maximum: {maximum}");
+            /// 3. nejmenší hodnotu
+            var minimum = numbers.Min();
+            Console.WriteLine($"Minimum: {minimum}");
+            /// 4. průměr
+            var prumer = numbers.Average();
+            Console.WriteLine($"Prumer: {prumer}");
+            /// 5. kolik obsahuje pole kladných čísel
+            //var pocetKladnych = numbers.Where(n => n >= 0).Count();
+            var pocetKladnych = numbers.Count(n => n >= 0); //taky možnost zápisu
+            Console.WriteLine($"pocet kladnych: {pocetKladnych}");
+            /// 6. kolik obsahuje pole záporných čísel
+            var pocetZapornych = numbers.Where(n => n < 0).Count();
+            Console.WriteLine($"pocet zapornych: {pocetZapornych}");
+            /// 7. sumu všech hodnot
+            var sumaVsech = numbers.Count();
+            Console.WriteLine($"Suma: {sumaVsech}");
+            /// 8. sumu kladných hodnot
+            var sumaKladnych = numbers.Where(n => n > 0).Sum();
+            Console.WriteLine($"Suma kladnych: {sumaKladnych}");
+            /// 9. seradit od nejmenší po největší, přeskočit první 3 a sečíst zbytek
+            int result = numbers.OrderBy(n => n).Skip(3).Sum();
+            Console.WriteLine($"vysledek: {result}");
+            /// 10. seradit absolutní hodnoty od největší po nejmenší a vypsat první tři
+            var result10 = numbers.OrderByDescending(x => Math.Abs(x)).Take(3);
+            Console.WriteLine($"MaxAbs: {string.Join(", ", result10)}");
+            */
+            #endregion
+
+            //Select - transformace
+            #region Select
+
+            int[] numbers = { -2079, -498, 2920, -1856, 332, -2549, -674, -120, -992, 2782, 320, -524, 135, 952, 1868, 2509, -230, -138, -904, -480 };
+
+            //var result = numbers.Select(number => number + 10);
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //úkol - vypsat absolutní hodnoty pro numbers
+            var result = numbers.Select(n => Math.Abs(n));
+            Console.WriteLine(string.Join(" ", result));
+
+            #endregion
+
 
             Console.ReadLine();
 
